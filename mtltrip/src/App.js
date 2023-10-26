ab3d41101b753b1db959562bb51c2bdb888a3db0
Andreia Basile Arruda <andreia.basile@gmail.com>
2023-10-26 18:45:13 -0400

    MTL-0001: styling fixes

--- mtltrip/src/App.js
+++ mtltrip/src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div class="main-container">
-        <header>I'm a 40px tall header</header>
+      <div class="body">
+        <header>I'm a 80px tall header and I am now fixed</header>
 
         <main>
           <p>
@@ -82,9 +82,69 @@ function App() {
             nunc volutpat tincidunt ac in arcu. Quisque pulvinar consequat
             magna, in ornare est faucibus ut.
           </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            diam tellus, tincidunt tristique dolor in, cursus hendrerit nisl.
+            Proin hendrerit ipsum sed lacus tempor mattis. Fusce at dapibus
+            arcu. Suspendisse non diam lobortis, ultrices libero in, maximus
+            justo. Aenean iaculis, justo vehicula pharetra eleifend, turpis nisi
+            accumsan orci, id pellentesque justo lectus in mauris. Etiam auctor
+            risus semper pellentesque venenatis. Quisque ipsum dolor, fringilla
+            aliquet ipsum vitae, pulvinar fringilla leo. Vestibulum porttitor
+            ipsum eu vehicula luctus. Suspendisse vestibulum tincidunt risus,
+            eget gravida purus fermentum nec. Maecenas sed accumsan nunc. Ut
+            pretium bibendum ante vitae tincidunt. Vestibulum a sapien dictum
+            nunc volutpat tincidunt ac in arcu. Quisque pulvinar consequat
+            magna, in ornare est faucibus ut.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            diam tellus, tincidunt tristique dolor in, cursus hendrerit nisl.
+            Proin hendrerit ipsum sed lacus tempor mattis. Fusce at dapibus
+            arcu. Suspendisse non diam lobortis, ultrices libero in, maximus
+            justo. Aenean iaculis, justo vehicula pharetra eleifend, turpis nisi
+            accumsan orci, id pellentesque justo lectus in mauris. Etiam auctor
+            risus semper pellentesque venenatis. Quisque ipsum dolor, fringilla
+            aliquet ipsum vitae, pulvinar fringilla leo. Vestibulum porttitor
+            ipsum eu vehicula luctus. Suspendisse vestibulum tincidunt risus,
+            eget gravida purus fermentum nec. Maecenas sed accumsan nunc. Ut
+            pretium bibendum ante vitae tincidunt. Vestibulum a sapien dictum
+            nunc volutpat tincidunt ac in arcu. Quisque pulvinar consequat
+            magna, in ornare est faucibus ut.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            diam tellus, tincidunt tristique dolor in, cursus hendrerit nisl.
+            Proin hendrerit ipsum sed lacus tempor mattis. Fusce at dapibus
+            arcu. Suspendisse non diam lobortis, ultrices libero in, maximus
+            justo. Aenean iaculis, justo vehicula pharetra eleifend, turpis nisi
+            accumsan orci, id pellentesque justo lectus in mauris. Etiam auctor
+            risus semper pellentesque venenatis. Quisque ipsum dolor, fringilla
+            aliquet ipsum vitae, pulvinar fringilla leo. Vestibulum porttitor
+            ipsum eu vehicula luctus. Suspendisse vestibulum tincidunt risus,
+            eget gravida purus fermentum nec. Maecenas sed accumsan nunc. Ut
+            pretium bibendum ante vitae tincidunt. Vestibulum a sapien dictum
+            nunc volutpat tincidunt ac in arcu. Quisque pulvinar consequat
+            magna, in ornare est faucibus ut.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            diam tellus, tincidunt tristique dolor in, cursus hendrerit nisl.
+            Proin hendrerit ipsum sed lacus tempor mattis. Fusce at dapibus
+            arcu. Suspendisse non diam lobortis, ultrices libero in, maximus
+            justo. Aenean iaculis, justo vehicula pharetra eleifend, turpis nisi
+            accumsan orci, id pellentesque justo lectus in mauris. Etiam auctor
+            risus semper pellentesque venenatis. Quisque ipsum dolor, fringilla
+            aliquet ipsum vitae, pulvinar fringilla leo. Vestibulum porttitor
+            ipsum eu vehicula luctus. Suspendisse vestibulum tincidunt risus,
+            eget gravida purus fermentum nec. Maecenas sed accumsan nunc. Ut
+            pretium bibendum ante vitae tincidunt. Vestibulum a sapien dictum
+            nunc volutpat tincidunt ac in arcu. Quisque pulvinar consequat
+            magna, in ornare est faucibus ut.
+          </p>
         </main>
 
-        <footer>I'm a 30px tall footer</footer>
+        <footer>I'm a 30px tall footer and I am now fixed</footer>
       </div>
     </>
   );
