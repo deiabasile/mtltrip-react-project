844fe385e1d22332377dfb7b8509a66997858837
Andreia Basile Arruda <andreia.basile@gmail.com>
2023-11-06 18:09:25 -0500

    Fix button & few configs

--- mtltrip/src/App.jsx
+++ mtltrip/src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import Button from "./components/button/index";
+// import { Suspense } from "react";
+// import { useTranslation } from "react-i18next";
 
 function App() {
   return (
