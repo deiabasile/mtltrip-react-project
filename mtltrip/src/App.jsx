0a9634aff89c2ca94ec43bcc169c7c661547b8a6
Andreia Basile Arruda <andreia.basile@gmail.com>
2023-11-15 21:19:59 -0500

    MTLTRIP-0004: linting files

--- mtltrip/src/App.jsx
+++ mtltrip/src/App.jsx
@@ -1,29 +1,30 @@
-import "./App.css";
-import Button from "./components/button/index";
+import React from 'react';
+import './App.css';
+import Button from './components/button';
 
 function App() {
   return (
-    <div class="layoutWrapper">
-      <header></header>
+    <div className="layoutWrapper">
+      <header />
 
       <main>
         <Button
           name="name test 2"
           id="123"
-          onButtonClick={() => alert("Hello world!")}
+          onButtonClick={() => alert('Hello world 1!')}
           label="Button"
           type="primary"
         />
         <Button
           name="name test 2"
           id="123"
-          onButtonClick={() => alert("Hello world!")}
+          onButtonClick={() => alert('Hello world 2!')}
           label="Button"
           type="secondary"
         />
       </main>
 
-      <footer></footer>
+      <footer />
     </div>
   );
 }
--- mtltrip/src/components/button/index.jsx
+++ mtltrip/src/components/button/index.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // Styles
-import classes from "./Button.module.scss";
+import classes from './Button.module.scss';
 
-function Button({ name, id, onButtonClick, label, type, isDisabled }) {
+function Button({
+  name, id, onButtonClick, label, type, isDisabled,
+}) {
   return (
     <button
-      className={type === "primary" ? classes.primary : classes.secondary}
+      className={type === 'primary' ? classes.primary : classes.secondary}
       disabled={isDisabled}
       id={id}
       name={name}
@@ -17,5 +20,23 @@ function Button({ name, id, onButtonClick, label, type, isDisabled }) {
     </button>
   );
 }
+
+Button.defaultProps = {
+  id: '',
+  isDisabled: false,
+  label: '',
+  name: '',
+  onButtonClick: () => null,
+  type: 'Primary',
+};
+
+Button.propTypes = {
+  id: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  label: PropTypes.string,
+  name: PropTypes.string,
+  onButtonClick: PropTypes.func,
+  type: PropTypes.string,
+};
 
 export default Button;
